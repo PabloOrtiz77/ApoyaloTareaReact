48f600365001f02d4f0ab12a7c9b662d1abcd0d4
PabloOrtiz <144179838+PabloOrtiz77@users.noreply.github.com>
2023-12-14 18:15:58 -0300

    Se agergo boton para aumentar la edad

--- app/src/App.js
+++ app/src/App.js
@@ -12,13 +12,14 @@ function App() {
     <div style={containerStyle}>
       <Info firstName="Jane" lastName="Doe" age={45} hairColor="Black" />
       <Info firstName="John" lastName="Smith" age={88} hairColor="Brown" />
-      <Info
+      {/* <Info
         firstName="Millard"
         lastName="Fillmore"
         age={50}
         hairColor="Brown"
       />
       <Info firstName="Maria" lastName="Smith" age={62} hairColor="Brown" />
+      */}
     </div>
   );
 }
--- app/src/components/Info.jsx
+++ app/src/components/Info.jsx
@@ -1,15 +1,35 @@
-import { Component } from "react";
+import React, { Component } from "react";
 
-class Info extends Component{
-    render(){
-        const {firstName,lastName,age,hairColor}=this.props
-        return(
-            <>
-            <h1>{lastName},{firstName}</h1>
-            <p>Age:{age}</p>
-            <p>Hair color:{hairColor}</p>
-            </>
-        );
-    }
+class Info extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: props.firstName,
+      lastName: props.lastName,
+      age: props.age,
+      hairColor: props.hairColor,
+    };
+  }
+
+  aumentar = () => {
+    // Actualiza el estado incrementando la edad en 1
+    this.setState((prevState) => ({
+      age: prevState.age + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <h1>{this.state.lastName}, {this.state.firstName}</h1>
+        <p>Age: {this.state.age}</p>
+        <p>Hair color: {this.state.hairColor}</p>
+        <button onClick={this.aumentar}>
+          Birthday Button for {this.state.firstName} {this.state.lastName} 
+        </button>
+      </>
+    );
+  }
 }
+
 export default Info;
