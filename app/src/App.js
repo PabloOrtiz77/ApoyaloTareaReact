02c1ec1b7b900ea412e42f76701f788f0a6d2335
PabloOrtiz <144179838+PabloOrtiz77@users.noreply.github.com>
2023-12-20 14:47:46 -0300

    Agregando componente funcional

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Info from "./components/Info";
+import PersonCard from "./components/Info";
 
 function App() {
   const containerStyle = {
@@ -10,16 +10,25 @@ function App() {
 
   return (
     <div style={containerStyle}>
-      <Info firstName="Jane" lastName="Doe" age={45} hairColor="Black" />
-      <Info firstName="John" lastName="Smith" age={88} hairColor="Brown" />
-      {/* <Info
+      <PersonCard firstName="Jane" lastName="Doe" age={45} hairColor="Black" />
+      <PersonCard
+        firstName="John"
+        lastName="Smith"
+        age={88}
+        hairColor="Brown"
+      />
+      <PersonCard
         firstName="Millard"
         lastName="Fillmore"
         age={50}
         hairColor="Brown"
       />
-      <Info firstName="Maria" lastName="Smith" age={62} hairColor="Brown" />
-      */}
+      <PersonCard
+        firstName="Maria"
+        lastName="Smith"
+        age={62}
+        hairColor="Brown"
+      />
     </div>
   );
 }
--- app/src/components/Info.jsx
+++ app/src/components/Info.jsx
@@ -1,35 +1,47 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Info extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      firstName: props.firstName,
-      lastName: props.lastName,
-      age: props.age,
-      hairColor: props.hairColor,
-    };
-  }
+const PersonCard=(props)=>{
+  return(
+    <>
+      <h1> {props.lastName}, {props.firstName}</h1>
+      <p>Age:{props.age}</p>
+      <p>Hair color:{props.hairColor}</p>
+    </>
+  );
+};
 
-  aumentar = () => {
-    // Actualiza el estado incrementando la edad en 1
-    this.setState((prevState) => ({
-      age: prevState.age + 1,
-    }));
-  };
+export default PersonCard;
 
-  render() {
-    return (
-      <>
-        <h1>{this.state.lastName}, {this.state.firstName}</h1>
-        <p>Age: {this.state.age}</p>
-        <p>Hair color: {this.state.hairColor}</p>
-        <button onClick={this.aumentar}>
-          Birthday Button for {this.state.firstName} {this.state.lastName} 
-        </button>
-      </>
-    );
-  }
-}
+// class Info extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       firstName: props.firstName,
+//       lastName: props.lastName,
+//       age: props.age,
+//       hairColor: props.hairColor,
+//     };
+//   }
 
-export default Info;
+//   aumentar = () => {
+//     // Actualiza el estado incrementando la edad en 1
+//     this.setState((prevState) => ({
+//       age: prevState.age + 1,
+//     }));
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <h1>{this.state.lastName}, {this.state.firstName}</h1>
+//         <p>Age: {this.state.age}</p>
+//         <p>Hair color: {this.state.hairColor}</p>
+//         <button onClick={this.aumentar}>
+//           Birthday Button for {this.state.firstName} {this.state.lastName} 
+//         </button>
+//       </>
+//     );
+//   }
+// }
+
+// export default Info;
